22f2a7d043542871a10b38ceec339f7ece949e66
swamaki <swamaki@gmail.com>
2023-09-23 08:45:26 -0400

    renamed files

---
+++ 2_example.py
@@ -0,0 +1,7 @@
+
+#!/usr/bin/env python3
+
+friends = ['john', 'pat', 'gary', 'michael']
+for i, name in enumerate(friends):
+    print ("iteration {iteration} is {name}".format(iteration=i, name=name))
+
